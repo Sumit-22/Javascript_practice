d6bed58fc58a7017a5e3a57057fa3bfcfb43cb78
Sumit-22 <145086126+Sumit-22@users.noreply.github.com>
2024-03-30 04:48:44 +0000

    container config new

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,14 @@
 console.log("Hi Sumit")
+const accountId = 14450
+let accountEmail = "sumitgaurav049@gmail.com"
+var accountPassword ="12345"
+accountCity = " Jaipur " //normally it's not preferred
+let accountState
+// accountId = 2 as constant can't be reassigned
+accountEmail="241243@gmail.com"
+accountPassword ="212121"
+accountCity="Banglore"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//block and functional scope problem in javascript :- due to var  so, remember always use let in place of var
+
