743d0ccf0f72e75a56bdda463ffa3c203dd5a5fa
Sumit-22 <145086126+Sumit-22@users.noreply.github.com>
2024-03-30 04:53:39 +0000

    container config new

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -4,7 +4,7 @@ let accountEmail = "sumitgaurav049@gmail.com"
 var accountPassword ="12345"
 accountCity = " Jaipur " //normally it's not preferred
 let accountState
-// accountId = 2 as constant can't be reassigned
+// accountId = 2 as constant can't be reassigned.
 accountEmail="241243@gmail.com"
 accountPassword ="212121"
 accountCity="Banglore"
