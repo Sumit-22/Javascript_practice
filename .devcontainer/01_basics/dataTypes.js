bde85de1c960f1d02a0ada595afd4c14c38c7988
Sumit-22 <145086126+Sumit-22@users.noreply.github.com>
2024-04-05 12:07:11 +0000

    datatypes

--- .devcontainer/01_basics/dataTypes.js
+++ .devcontainer/01_basics/dataTypes.js
@@ -1,4 +1,21 @@
 "use strict"; // treat all js code as newer version
 //alert("hello")  // we are using node js not browser
 
-console.log(3+3)
+console.log(3+3)  // code readability should be high
+//consult ecma standard and mdn reference  
+//primitive datatypes:-
+let name = "hitesh" //String => ""
+let age = 18 //number => 2 ^53 
+//bigint
+//null =>standalone value -> representation of eempty value
+//undefined =>values are not assigned
+//Symbols =>unique
+let isLoggedIn = false //boolean
+
+//object
+console.log(typeof age);
+
+console.log(typeof null); // wee get object
+
+console.log(typeof undefined); //undefined
+
