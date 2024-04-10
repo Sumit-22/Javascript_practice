2e0b072582acdd857427d4f9cb3af3ecb94cb533
Sumit-22 <145086126+Sumit-22@users.noreply.github.com>
2024-04-10 16:20:27 +0000

    tricky conversion

--- .devcontainer/01_basics/conversionOperation.js
+++ .devcontainer/01_basics/conversionOperation.js
@@ -25,4 +25,37 @@ let StringNumber =String(score2)
 
 console.log(StringNumber); 
 console.log(typeof (StringNumber));  
+//************************************************* Operations **************************************
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2**2);
+//console.log(2%3);
+ 
+let str1="hello"
+let str2=" hitesh"
+console.log(str1+str2)
+
+console.log("1"+2); //12 //consult    https://tc39.es/ecma262/#sec-toprimitive
+console.log("1"+2+2); //122
+console.log(1+"2") //12
+console.log(1+2+"2") //32
+console.log("1"+"2") //12
+
+// tricky conversion(not use in real life)
+console.log(+true); //1 and we get error in case of console.log(true+); 
+//0 if console.log(+"");
+
+let num1,num2,num3
+//we don' generally use
+num1=num2=num3=2+2
+
+//prefix and postfix
+//consult https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 
