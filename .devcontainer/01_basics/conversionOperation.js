a9e563368d326e10e11bf6b1b70748499729fd73
Sumit-22 <145086126+Sumit-22@users.noreply.github.com>
2024-04-10 15:49:33 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueNumber =Number(score)
+
+console.log(typeof (valueNumber))
+let score1 = "33abc" //if it's null ,print 0 ,if true then 1 and false then 0 , if any string, then NaN
+let valueNumber1 =Number(score1)
+
+console.log(valueNumber)  //prints NaN
+console.log(typeof (valueNumber1))  //number
+
+let isLoggedIn =1 // then true ,0 then false , "" then false and any string then true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let score2 = 33
+console.log(typeof (score2))
+
+let StringNumber =String(score2)
+
+console.log(StringNumber); 
+console.log(typeof (StringNumber));  
+
